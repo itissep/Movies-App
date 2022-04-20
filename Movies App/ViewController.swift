123aa52856e47a9f3623886a1d2e7339ab007e6a
Someone Else's Problem <someonenewish@yandex.ru>
2022-04-20 20:33:47 +0300

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by The GORDEEVS on 20.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
